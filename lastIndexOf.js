41c364fd6d5d524b23ac603c849373b9aecc691f
Rene Wang <rene.wang1@hotmail.com>
2020-10-14 23:44:09 +0000

    function that returns last index of value in array or -1 if not found

---
+++ lastIndexOf.js
@@ -0,0 +1,9 @@
+const lastIndexOf = function(array, value){
+  for (let i = array.length - 1; i >= 0; i--){
+    if (array[i] === value){
+      return i
+    }
+  }
+  return -1
+}
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1))
